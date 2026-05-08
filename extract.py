dab02c131eeea93832e52bc286108b93104c8e9b
RuvejdaHamza <ruvejda.hamza@student.uni-pr.edu>
2026-05-08 21:14:57 +0000

    Implement message extraction and decoding from pixels

--- extract.py
+++ extract.py
@@ -32,5 +32,60 @@ random.shuffle(shuffledIndices)
 
 
 
+# 4. LEXO 14 BITAT E PARË (GJATËSIA E MESAZHIT)
+
+length_bits = []
+for i in range(14):
+    x = shuffledIndices[i] % dimensions[0]
+    y = shuffledIndices[i] // dimensions[0]
     
-print("\nMesazhi u ruajt edhe në 'extracted_secret.txt'")
+    colour_value = pixels[x, y][colourPlane]
+    p = format(colour_value, 'b').zfill(8)
+    length_bits.append(p[significantBit])
+
+length_bits_str = ''.join(length_bits)
+message_length = int(length_bits_str, 2)
+
+print(f"Gjatësia e mesazhit: {message_length} karaktere")
+
+
+# 5. LEXO BITAT E MESAZHIT
+
+message_bits = []
+start_index = 14
+num_bits_needed = message_length * 7
+
+for i in range(start_index, start_index + num_bits_needed):
+    x = shuffledIndices[i] % dimensions[0]
+    y = shuffledIndices[i] // dimensions[0]
+    
+    colour_value = pixels[x, y][colourPlane]
+    p = format(colour_value, 'b').zfill(8)
+    message_bits.append(p[significantBit])
+
+
+
+# 6. DEKODONO MESAZHIN
+
+
+message = ""
+for i in range(0, len(message_bits), 7):
+    byte_bits = ''.join(message_bits[i:i+7])
+    char_code = int(byte_bits, 2)
+    message += chr(char_code)
+
+
+
+# 7. SHFAQO DHE RUAJ MESAZHIN
+
+
+print("\n" + "="*50)
+print("MESAZHI I FSHUR:")
+print("="*50)
+print(message)
+print("="*50)
+
+with open("extracted_secret.txt", "w", encoding="utf-8") as f:
+    f.write(message)
+    
+print("\nMesazhi u ruajt edhe në 'extracted_secret.txt'")print("\nMesazhi u ruajt edhe në 'extracted_secret.txt'")
